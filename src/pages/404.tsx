287060970164d7f31d25f51401efd6bdc493985d
Stanislau Melnik <95521674+StanislavMelnik1991@users.noreply.github.com>
2023-10-30 18:52:15 +0300

    Not found (#5)
    * fix: language menu position
    
    * fix: not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Language, acceptedLanguages } from "@shared/constants/language";
 import { NotFound } from "widgets/404/NotFound";
 import Head from "next/head";
+import { Header } from "widgets/Header/Header";
 
 export async function getStaticProps({ locale }: { locale: Language }) {
   return {
@@ -22,6 +23,7 @@ export default function Custom404() {
       <Head>
         <title>{t("pages.notFound")}</title>
       </Head>
+      <Header />
       <NotFound />
     </>
   );
